256256c49a329f6a133bdcce700f3c5c480b6c97
Cancellier27 <filipecancelliercosta@gmail.com>
2020-09-13 19:53:15 +0100

    creating carousel logic

--- src/config/carousel.js
+++ src/config/carousel.js
@@ -1,1 +1,15 @@
 // File for creation of carousel animation
+const carouselSlider = document.querySelector(".carouselSlider")
+const carouselImages = document.querySelectorAll(".carouselSlider img")
+
+function carouselLoop (slider) {
+  let counter = 1
+  const imgSize = carouselImages[counter].clientWidth + 24;
+  setInterval(() => {
+    console.log(imgSize, counter)
+    slider.style.transform = `translateX(-${imgSize * counter}px)`
+    counter += 1
+  } ,2000)
+}
+
+carouselLoop(carouselSlider)
