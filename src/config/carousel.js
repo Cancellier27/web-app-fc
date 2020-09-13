5d552d51956cec962758da2a12792a5c760bece1
Cancellier27 <filipecancelliercosta@gmail.com>
2020-09-13 20:26:33 +0100

    creating carousel slider

--- src/config/carousel.js
+++ src/config/carousel.js
@@ -2,14 +2,27 @@
 const carouselSlider = document.querySelector(".carouselSlider")
 const carouselImages = document.querySelectorAll(".carouselSlider img")
 
+let counter = 0
+const imgSize = carouselImages[counter].clientWidth + 24;
+
 function carouselLoop (slider) {
-  let counter = 1
-  const imgSize = carouselImages[counter].clientWidth + 24;
+  
+
   setInterval(() => {
     console.log(imgSize, counter)
-    slider.style.transform = `translateX(-${imgSize * counter}px)`
+    slider.style.transition = `transform 1000ms ease-in-out`
+    slider.style.transform = `translateX(-${imgSize * counter + 1}px)`
     counter += 1
-  } ,2000)
+
+    if (carouselImages[counter].id === "lastImg") {
+      console.log("acabou")
+      counter = 1
+      slider.style.transition = "none" 
+      slider.style.transform = `translateX(0px)`
+    }
+  } ,3000)
 }
+
+
 
 carouselLoop(carouselSlider)
