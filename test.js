dfc46609c7316b1057930c03fb93d09c8670ff5d
Cancellier27 <filipecancelliercosta@gmail.com>
2020-09-27 23:49:01 +0100

    splice and slice codewars

--- test.js
+++ test.js
@@ -1,19 +1,7 @@
-function incrementString (strng) {
-  let numbers = '0123456789'
-  if (!numbers.includes(strng[strng.length - 1])) return `${strng}1`
-  else {
-    let newStrng = strng.split('')
-    let m = parseFloat(newStrng[[strng.length - 1]]) + 1
-    newStrng.pop()
-    newStrng.push(`${m}`)
-    return newStrng.join('')
-    
-  }
+let arr = [1, 2, 3, 4, 5]
 
-}
+console.log(arr.slice(0, 2))
+console.log(arr)
 
-
-
-
-console.log(incrementString("foobar000"));
-console.log(incrementString("foo"));
+console.log(arr.splice(0, 2))
+console.log(arr)
