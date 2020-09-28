07d6e2a9f559bf30d792301beb5f05d919662038
Cancellier27 <filipecancelliercosta@gmail.com>
2020-09-28 19:01:15 +0100

    rebuilding carousel

--- src/config/carousel.js
+++ src/config/carousel.js
@@ -1,47 +1,50 @@
 // File for creation of carousel animation
-const carouselSlider = document.querySelector(".carouselSlider")
+const carouselContainer = document.querySelector(".carouselContainer")
+const carouselSlider = document.querySelectorAll(".carouselSlider")
 
-let carouselImages = document.querySelectorAll(".carouselSlider img")
-
+let imgSlide = document.querySelectorAll(".slidesImg")
 let counter = 1
-let slideInterval
+// let slideInterval
 
-const imgSize = carouselImages[counter].clientWidth + 14;
-const interval = 1000;
-carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`
+const firstCloneSlide = imgSlide[0].cloneNode(true)
+const lastCloneSlide = imgSlide[imgSlide.length - 1].cloneNode(true)
 
-
-const carouselLoop = () => {
-  slideInterval = setInterval(() => {
-    moveToNextImg()
-  }, interval)
-}
-
-const getCarouselSlide = () => document.querySelectorAll(".carouselSlider img");
+// const imgSize = carouselImages[counter].clientWidth + 10;
+// const interval = 1000;
+// carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`
 
 
-carouselSlider.addEventListener('transitionend', () => {
-  slides = getCarouselSlide();
-  if (slides[counter].id === "lastImg") {
-    carouselSlider.style.transition = 'none';
-    counter = 1;
-    carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`;
-  }
+// const carouselLoop = () => {
+//   slideInterval = setInterval(() => {
+//     moveToNextImg()
+//   }, interval)
+// }
 
-  if (slides[counter].id === "firstImg") {
-    carouselSlider.style.transition = 'none';
-    counter = slides.length - 2;
-    carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`;
-  }
-});
+// const getCarouselSlide = () => document.querySelectorAll(".slidesImg");
 
 
+// carouselSlider.addEventListener('transitionend', () => {
+//   slides = getCarouselSlide();
+//   if (slides[counter].id === "lastImg") {
+//     carouselSlider.style.transition = 'none';
+//     counter = 1;
+//     carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`;
+//   }
+
+//   if (slides[counter].id === "firstImg") {
+//     carouselSlider.style.transition = 'none';
+//     counter = slides.length - 2;
+//     carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`;
+//   }
+// });
 
 
-const moveToNextImg = () => {
-  counter++
-  carouselSlider.style.transition = `700ms ease-out`
-  carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`
-}
 
-// carouselLoop()
+
+// const moveToNextImg = () => {
+//   counter++
+//   carouselSlider.style.transition = `700ms ease-out`
+//   carouselSlider.style.transform = `translateX(${-imgSize * counter}px)`
+// }
+
+// // carouselLoop()
