2a39c3491728be7ab3674c549b1295cc24833221
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-11-03 22:27:26 +0000

    initiating creation of interface for changing the theme of the web site to light theme

---
+++ src/config/lightTheme.js
@@ -0,0 +1,13 @@
+let watch = 'black'
+
+function changeColor() {
+  if (watch === 'black') {
+    document.body.style.backgroundImage = 'url("./src/assets/backgroundWhite.png")'
+    watch = 'white'
+  } else {
+    document.body.style.backgroundImage = 'url("./src/assets/backgroundAll.png")'
+    watch = 'black'
+  }
+
+
+}
