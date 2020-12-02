ee4d930f6140585edda0c32ee161db81c38da7a5
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-12-02 20:41:13 +0000

    update date file

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,18 +1,6 @@
-function newElement(tagName, className) {
-  const elem = document.createElement(tagName)
-  elem.className = className
-  return elem
-}
-
-
-function todayDate() {
+window.addEventListener("load", () => {
   const dayDate = new Date()
-  const forDate = document.querySelector('.forDate')
+  const containerDate = document.querySelector('.todayDate')
 
-  const dataTagElement = newElement('h3', 'todayDate')
-  forDate.innerHTML = dayDate.toDateString()
-
-  forDate.appendChild(dataTagElement)
-}
-
-todayDate()
+  containerDate.innerHTML = dayDate.toDateString()
+});
